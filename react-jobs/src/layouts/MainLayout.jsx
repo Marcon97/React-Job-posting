d2694fca3c9b1da7c998ae6c0ee12cddb235a771
Marcon97 <tinsaetariku2@gmail.com>
2024-10-06 12:18:40 +0300

    CRUD

--- react-jobs/src/layouts/MainLayout.jsx
+++ react-jobs/src/layouts/MainLayout.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/ReactToastify.css'
 import NavBar from '../components/NavBar'
 import { Outlet } from 'react-router-dom'
 const MainLayout = () => {
@@ -6,6 +8,7 @@ const MainLayout = () => {
     <>
     <NavBar/>
     <Outlet/>
+    <ToastContainer/>
     </>
   )
 }
